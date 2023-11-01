ab8771b4ddd5610aa0c1bce011444e2579d4768c
Pr0pHesyer <74277431+Pr0pHesyer@users.noreply.github.com>
2023-11-01 21:52:41 +0800

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aard3"
+include(":aard3-android-app")
+include(":slobj")
+include(":slobber")
